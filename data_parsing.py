6fd4ae2271f3e64716691974dfcb9cc2cafacf4e
KARISHMA RAMESH MANDYAM <karishma.mandyam@gmail.com>
2017-08-12 16:16:18 -0700

    separated training and test data with a 70:30 ratio

--- data_parsing.py
+++ data_parsing.py
@@ -8,11 +8,14 @@ with open('HR_comma_sep.csv', 'rb') as f:
 # print point['salary']
 
 # split raw data into training and test data: 70% and 30% respectively
+def separate_data(raw):
+    train_data = raw[0:10500]
+    test_data = raw[10500:]
+    return train_data, test_data
 
-
-#TODO: Sort through data
-#TODO: Github project
-#TODO: Separate the data into train and test sets
+#DONE: Sort through data
+#DONE: Github project
+#DONE: Separate the data into train and test sets
 #TODO: Write a classifier to determine whether or not a person will leave given some points
 #TODO: Write a front end that's pretty and easy to display
 #TASK: Given an employee, and a time frame, will they leave the company within that timeframe?
