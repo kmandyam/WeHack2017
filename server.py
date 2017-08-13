d25572d48b646e9168db01ebae15778e32d0f65d
KARISHMA RAMESH MANDYAM <karishma.mandyam@gmail.com>
2017-08-13 11:04:17 -0700

    added server tool to display files on localhost

---
+++ server.py
@@ -0,0 +1,27 @@
+# from bs4 import BeautifulSoup
+import urllib
+import csv
+from flask import Flask
+from flask import request
+from flask_cors import CORS
+import json
+from test import test_submission
+
+import os, sys, csv, urllib
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/loaddata" , methods=['GET', 'POST'])
+
+def load_trained_data():
+    # urlList = request.form['urlArray']
+
+    # urlList = json.loads(urlList)
+    # print urlList, "FIRST ONE"
+
+    # print "HELLO WORLD", len(urlList)
+    train_accuracy, test_accuracy = test_submission()
+    return "Training Accuracy: " + str(train_accuracy) + ", Testing Accuracy: " + str(test_accuracy)
+
+app.run()
--- test.py
+++ test.py
@@ -18,22 +18,22 @@ test_data = extract_features(test_separate)
 
 
 
-def test_logistic(self):
+def test_logistic():
     self.assertAlmostEqual(logistic(1),  0.7310585786300049)
     self.assertAlmostEqual(logistic(2),  0.8807970779778823)
     self.assertAlmostEqual(logistic(-1),  0.2689414213699951)
 
-def test_dot(self):
+def test_dot():
     d = dot([1.1,2,3.5], [-1,0.1,.08])
     self.assertAlmostEqual(d, -.62)
 
-def test_predict(self):
+def test_predict():
     model = [1,2,1,0,1]
     point = {'features':[.4,1,3,.01,.1], 'label': 1}
     p = predict(model, point)
     self.assertAlmostEqual(p, 0.995929862284)
 
-def test_accuracy(self):
+def test_accuracy():
     data = train_data
     a = accuracy(data, [0]*len(data))
 #     self.assertAlmostEqual(a, 0.751077514754)
@@ -45,10 +45,13 @@ def test_submission():
     print
     print
     # print predictions
-    print "Training Accuracy:", accuracy(train_data, predictions)
+    train_accuracy = accuracy(train_data, predictions)
+    print "Training Accuracy:", train_accuracy
     # print train_data
     predictions = [predict(model, p) for p in valid_data]
-    print "Validation Accuracy:", accuracy(valid_data, predictions)
+    valid_accuracy = accuracy(valid_data, predictions)
+    print "Validation Accuracy:", valid_accuracy
     print
+    return train_accuracy, valid_accuracy
 
 test_submission()
